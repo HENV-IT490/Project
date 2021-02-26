da8bae784106854ad2438beb1740459f2013ecb8
Wretchedpeasant <nikk70346@gmail.com>
2021-02-26 17:18:26 -0500

    making directories cleaner

---
+++ Logger/logAgent.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+require_once('../RabbitMQ/path.inc');
+require_once('../RabbitMQ/get_host_info.inc');
+require_once('../RabbitMQ/rabbitMQLib.inc');
+
+function sendLog($message){
+	//Thrown after an exception is captured
+	//$message is exception class getMessage()
+	// Append  Error Message + File Locaton + Line location of error
+	// Send message to log listner using rabbitMQ loglistener queue
+	// Need to use different ini  file to use that queue
+$client = new rabbitMQClient("loggerRabbitMQ.ini","logListener");
+
+/*$request = array();
+$request['type'] = "log";
+$request['username'] = "steve";
+$request['password'] = "password";
+$request['message'] = $message; 
+$client -> send_request($request); */
+$client -> send_request($message);
+echo "Log  sent \n";
+
+}
+
+
+
+
+
+?>
