5ea17f62895525f173910b4562d8c732a55ecad1
WretchedPeasant <nikk70346@gmail.com>
2021-05-06 19:33:56 -0400

    added some changes so shell script works as intended

--- testing/status.sh
+++ testing/status.sh
@@ -3,11 +3,12 @@ echo $OPPIP
 ping 25.2.97.87 -c5
 if [ $? -eq 0 ]
 then
-    sed -i "s/status=.*/status='slave'/g" ~/.bashrc
+    sed -i "s/STATUS=.*/STATUS='slave'/g" $HOME/.bashrc
     export STATUS="slave"
     echo "This is a slave now"
 else
-    sed -i "s/slavstatus=.*/status='master'/g" ~/.bashrc
+    sed -i "s/STATUS=.*/STATUS='master'/g" $HOME/.bashrc
     export STATUS="master"
     echo "This is a master now"
 fi
+exec bash
