6ce29a6e67d8e2557e060e6ce59d02f302e4badf
WretchedPeasant <nikk70346@gmail.com>
2021-05-10 19:27:33 -0400

    small changes to files

--- testing/apacheStatus.sh
+++ testing/apacheStatus.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ "$STATUS" == "HSB" ]
 then
-	sudo sed -i 's/*#ProxyPass/ProxyPass/g' /etc/apache2/sites-available/000-default.conf
+	sudo sed -i 's/.#ProxyPass/ProxyPass/g' /etc/apache2/sites-available/000-default.conf
 	sudo  systemctl reload apache2
 	sudo systemctl restart apache2
 elif [ "$STATUS" == "PRIMARY" ]
 then
-	   sudo sed -i 's/*ProxyPass/#ProxyPass/g' /etc/apache2/sites-available/000-default.conf
+	   sudo sed -i 's/ProxyPass/#ProxyPass/g' /etc/apache2/sites-available/000-default.conf
         sudo  systemctl reload apache2
         sudo systemctl restart apache2
 else
