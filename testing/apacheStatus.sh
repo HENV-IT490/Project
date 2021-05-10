15d202c7d52d7fd583b467c1bf74d55ac3b623be
WretchedPeasant <nikk70346@gmail.com>
2021-05-10 16:21:38 -0400

    apacheStatus debugging

--- testing/apacheStatus.sh
+++ testing/apacheStatus.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-if [ "$STATUS" == "PRIMARY" ]
+if [ "$STATUS" == "HSB" ]
 then
 	sudo sed -i "s/# ProxyPass/ProxyPass/g" /etc/apache2/sites-available/000-default.conf
 	sudo  systemctl reload apache2
 	sudo systemctl restart apache2
-else if [ "$STATUS" == "HSB" ]
+elif [ "$STATUS" == "PRIMARY" ]
+then
 	        sudo sed -i "s/ProxyPass/# ProxyPass/g" /etc/apache2/sites-available/000-default.conf
         sudo  systemctl reload apache2
         sudo systemctl restart apache2
+else
+	echo"Neither HSB or Primary"
 fi
 	
