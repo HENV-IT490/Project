b7d39c8c2dec5009f24d5b4ee24c7caa4c432cad
WretchedPeasant <nikk70346@gmail.com>
2021-05-10 19:45:44 -0400

    asdf

--- testing/apacheStatus.sh
+++ testing/apacheStatus.sh
@@ -6,7 +6,7 @@ then
 	sudo systemctl restart apache2
 elif [ "$STATUS" == "Primary" ]
 then
-	   sudo sed -i 's/*ProxyPass/#ProxyPass/g' /etc/apache2/sites-available/000-default.conf
+	   sudo sed -i 's/ProxyPass/#ProxyPass/g' /etc/apache2/sites-available/000-default.conf
         sudo  systemctl reload apache2
         sudo systemctl restart apache2
 else
