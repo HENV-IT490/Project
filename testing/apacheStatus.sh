8d0125831b9c2007dfb7e54f34657af7566bb8f4
WretchedPeasant <nikk70346@gmail.com>
2021-05-11 18:06:36 -0400

    rechaning this again...

--- testing/apacheStatus.sh
+++ testing/apacheStatus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$STATUS" == "HSB" ]
 then
-	sudo sed -i 's/*#Redirect/Redirect/g' /etc/apache2/sites-available/gethatrecipe.com.conf
+	sudo sed -i 's/.#Redirect/Redirect/g' /etc/apache2/sites-available/gethatrecipe.com.conf
 	sudo  systemctl reload apache2
 	sudo systemctl restart apache2
 elif [ "$STATUS" == "PRIMARY" ]
