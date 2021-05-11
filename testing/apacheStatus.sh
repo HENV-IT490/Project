0246f27983bf4bc5bded4a92dfea6a92441fa8c6
WretchedPeasant <nikk70346@gmail.com>
2021-05-11 14:20:53 -0400

    changed apachestatus.sh to look for our new conf file

--- testing/apacheStatus.sh
+++ testing/apacheStatus.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ "$STATUS" == "HSB" ]
 then
-	sudo sed -i 's/.#ProxyPass/ProxyPass/g' /etc/apache2/sites-available/000-default.conf
+	sudo sed -i 's/.#Redirect/Redirect/g' /etc/apache2/sites-available/gethatrecipe.com.conf
 	sudo  systemctl reload apache2
 	sudo systemctl restart apache2
 elif [ "$STATUS" == "PRIMARY" ]
 then
-	   sudo sed -i 's/ProxyPass/#ProxyPass/g' /etc/apache2/sites-available/000-default.conf
+	   sudo sed -i 's/Redirect/#Redirect/g' /etc/apache2/sites-available/gethatrecipe.com.conf
         sudo  systemctl reload apache2
         sudo systemctl restart apache2
 else
